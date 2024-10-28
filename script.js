745b09f1f3121e37a25adb9a305d207233970ca9
Rafael Arteaga <arteagarafat@gmail.com>
2024-10-27 23:08:16 -0500

    Adelanto del portafolio

---
+++ script.js
@@ -0,0 +1,17 @@
+let textoEfecto = document.getElementById("textoPresentacion");
+
+let maquinaEscribir = (text= "", tiempo=200, etiqueta="")=>{
+    let arrayCaracteres = text.split("");
+    etiqueta.innerHTML="";
+    let contador = 0;
+    let escribir = setInterval(function() {
+        etiqueta.innerHTML += arrayCaracteres[contador];
+        contador++;
+        if (contador === arrayCaracteres.length){    
+            clearInterval(escribir);
+        }
+    }, tiempo);
+    }
+
+    maquinaEscribir("Hola, soy Rafael Arteaga, desarrollador Junior, me gusta afrontar retos y resolver problemas.", 60, textoEfecto);
+        
